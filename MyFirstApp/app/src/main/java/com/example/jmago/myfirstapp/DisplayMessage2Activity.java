e1adc9ff5602bfc2258ebce501c287028abd440c
Jennifer Goodell <jmagood@bu.edu>
2016-10-01 18:26:46 -0400

    added a second line of text and a new activity

---
+++ MyFirstApp/app/src/main/java/com/example/jmago/myfirstapp/DisplayMessage2Activity.java
@@ -0,0 +1,25 @@
+package com.example.jmago.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DisplayMessage2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message2);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        ViewGroup layout =(ViewGroup) findViewById(R.id.activity_display_message2);
+        layout.addView(textView);
+    }
+}
--- MyFirstApp/app/src/main/java/com/example/jmago/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/jmago/myfirstapp/MainActivity.java
@@ -14,13 +14,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onResume() {
+        //TODO auto-generated method stub
+        super.onResume();
+    }
+    @Override
+    protected void onStart() {
+        //TODO auto-generated method stub
+        super.onStart();
+    }
+    @Override
+    protected void onPause() {
+        //TODO auto-generated method stub
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        //TODO auto-generated method stub
+        super.onStop();
+    }
+    @Override
+    protected void onRestart() {
+        //TODO auto-generated method stub
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        //TODO auto-generated method stub
+        super.onDestroy();
+    }
+
     /**Called when the user clicks the Send button*/
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
+        EditText editText = (EditText) findViewById(R.id.edit_message1);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
         //Do something in response to button
+    }
+    public void sendMessage2(View view) {
+        Intent intent = new Intent(this, DisplayMessage2Activity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message2);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
